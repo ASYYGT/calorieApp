df16fa4caa3b4217710425be4484438923eaff1a
ASSYGT <abdussametsoyyigit@gmail.com>
2022-05-05 22:43:33 +0300

    asd

--- src/main/java/vscode/App.java
+++ src/main/java/vscode/App.java
@@ -6,25 +6,19 @@ import static spark.Spark.post;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.function.DoubleBinaryOperator;
 
 import spark.ModelAndView;
 import spark.template.mustache.MustacheTemplateEngine;
 
 public class App {
     
-    
-    public String getGreeting() {
-        return "Hello world.";
-    }
-
-    public static double Hesapla(ArrayList<Double> array, double age, double size, double weight){
+    public static double calorie(ArrayList<Double> inputList, double age, double size, double weight){
 
 
         double alinmasiGerekenKalori = 66 + (9.6*weight) + (1.8 * size) + (4.7 * age);
-        array.add(age);
-        array.add(size);
-        array.add(weight);
+        inputList.add(age);
+        inputList.add(size);
+        inputList.add(weight);
         return alinmasiGerekenKalori;
     }
 
@@ -32,29 +26,29 @@ public class App {
         port(getHerokuAssignedPort());
         get("/", (req, res) -> "Merhaba Dünya");
         post("/compute", (req, res) -> {
-            
-            double yasInput = Integer.parseInt(req.queryParams("input1")) ;
-            double sigaraInput = Integer.parseInt(req.queryParams("input2")) ;
-            double sporInput = Integer.parseInt(req.queryParams("input3")) ;
+
+            double ageI = Integer.parseInt(req.queryParams("input1")) ;
+            double sizeI = Integer.parseInt(req.queryParams("input2")) ;
+            double weightI = Integer.parseInt(req.queryParams("input3")) ;
            
            
             java.util.ArrayList<Double> inputList = new java.util.ArrayList<>();
             
-                inputList.add(yasInput);
-                inputList.add(sigaraInput);
-                inputList.add(sporInput);
+                inputList.add(ageI);
+                inputList.add(sizeI);
+                inputList.add(weightI);
             System.out.println(inputList);
-            double yas = inputList.get(0);
-            double sigara = inputList.get(1);
-            double spor = inputList.get(2);
-            double result = App.Hesapla(inputList, yasInput, sigaraInput, sporInput);
+            double age = inputList.get(0);
+            double size = inputList.get(1);
+            double weight = inputList.get(2);
+            double result = App.calorie(inputList, ageI, sizeI, weightI);
             
             
             Map<String, Double> map = new HashMap<String, Double>();
             map.put("result", result );
-            map.put("yas", yas );
-            map.put("sigara", sigara );
-            map.put("spor", spor );
+            map.put("yas", age );
+            map.put("boy", size );
+            map.put("ağırlık", weight );
             return new ModelAndView(map, "compute.mustache");
         }, new MustacheTemplateEngine());
         get("/compute",
@@ -71,6 +65,6 @@ public class App {
         if (processBuilder.environment().get("PORT") != null) {
             return Integer.parseInt(processBuilder.environment().get("PORT"));
         }
-        return 4567; // return default port if heroku-port isn't set (i.e. on localhost)
+        return 4567;
     }
 }
--- src/test/java/vscode/AppTest.java
+++ src/test/java/vscode/AppTest.java
@@ -6,9 +6,38 @@ package vscode;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 class AppTest {
-    @Test void appHasAGreeting() {
-        App classUnderTest = new App();
-        assertNotNull(classUnderTest.getGreeting(), "app should have a greeting");
+    public ArrayList<Double> array  = new ArrayList<>();
+    
+    public void testEqualsArray(){
+        
+       
+        ArrayList<Double> array1 = new ArrayList<>(Arrays.asList(1.0,1.0,1.0)); 
+        App.calorie(array, 1, 1, 1);
+       
+        assertEquals(array, array1);
+       
     }
+    @Test
+    public void testNotEqualsArray(){
+       
+        ArrayList<Double> array1 = new ArrayList<>(Arrays.asList(1.0,1.0,1.0)); 
+        App.calorie(array, 2, 2, 2);
+       
+        assertNotEquals(array, array1);
+    }
+    @Test
+    public void testNotNull(){
+       
+        App.calorie(array, 1, 1, 1);
+        assertNotNull(array);
+    }
+    @Test
+    public void testNull(){
+        
+        assertNotNull(array);
+    }
+
 }
